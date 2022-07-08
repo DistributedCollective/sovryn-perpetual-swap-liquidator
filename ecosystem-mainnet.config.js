d2fae8b19c824d8d7c0ef16d086b4de8fcb4741e
Tudor Constantin <tudorgalt@gmail.com>
2022-07-08 18:30:35 +0300

    added the gnosis safe as owner

--- ecosystem-mainnet.config.js
+++ ecosystem-mainnet.config.js
@@ -9,14 +9,14 @@ module.exports = {
       PERP_ID: "0x369d7c01e026e750d616303e0fa4ac262c55e4ebe19a54cbf15d814b03b1182b",
       IDX_ADDR_START: 0,
       NUM_ADDRESSES: 4,
-      PERP_NAME: 'MAINNET-BTCUSD',
+      PERP_NAME: 'MAINNET-BTCUSD-LIQ',
       MANAGER_ADDRESS: "0x86f586dc122d31E7654f89eb566B779C3D843e22",
       TOKEN_ADDRESS: "0x6a7F2d2e5D5756729e875c8F8fC254448E763Fdf",
       NODE_URLS: '["https://bsc.sovryn.app/testnet","https://bsc-dataseed1.binance.org/","https://bsc-dataseed2.binance.org/","https://bsc-dataseed3.binance.org/","https://bsc-dataseed4.binance.org/","https://bsc-dataseed1.defibit.io/","https://bsc-dataseed2.defibit.io/","https://bsc-dataseed3.defibit.io/", "https://bsc-dataseed4.defibit.io/", "https://bsc-dataseed1.ninicoin.io/", "https://bsc-dataseed2.ninicoin.io/", "https://bsc-dataseed3.ninicoin.io/", "https://bsc-dataseed4.ninicoin.io/"]',
       PUBLIC_NODE_PROVIDER: 'https://bsc-dataseed1.binance.org/',
       HEARTBEAT_LISTENER_URL: "https://thenurse.prforge.com/api/heartbeats",
       HEARTBEAT_SHOULD_RESTART_URL: "https://thenurse.prforge.com/api/heartbeats/should-restart",
-      OWNER_ADDRESS: "0x6a7F2d2e5D5756729e875c8F8fC254448E763Fdf", //treasury address
+      OWNER_ADDRESS: "0xC12742a5b12F76622350fB7558cc2B4A9c7de8e0", //gnosis safe
       TESTNET: false,
       DB_NAME: "liquidator_mainnet_btcusd.db",
       BLOCK_EXPLORER: "https://www.bscscan.com/",
